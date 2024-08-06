7a05de18de3dfaeaf106f47a80a7ec3643ef8c54
Ido Yotvat <idoy@moveo.co.il>
2024-08-06 14:11:40 +0300

    Clean and arrange Code

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,22 +1,13 @@
-// import bcrypt from 'bcryptjs';
 import { LoginData, LoginResponse } from "../data/types";
 import { httpService } from './http.service';
 import CryptoJS from 'crypto-js'
 
-
-// const saltRounds = 10
 const secretKey = import.meta.env.SECRET_KEY || 'your-secret-key'
 
 export const loginUser = async (data: LoginData): Promise<LoginResponse> => {
 
-    //bcrypt
-    // const hashedPassword = await bcrypt.hash(data.password, saltRounds)
-    // console.log(hashedPassword)
-    // const encryptedData = { ...data, password: hashedPassword }
-
     //cryptojs
     const encryptedPassword = CryptoJS.AES.encrypt(data.password, secretKey).toString()
-    console.log(encryptedPassword)
     const encryptedData = { ...data, password: encryptedPassword }
     return httpService.post('user/login', encryptedData)
 }
